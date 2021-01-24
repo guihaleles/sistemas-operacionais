e7b46c9255b5a2d85204c28a85c3cc54870e3d82
Victor Moraes <victor.moraes@enacom.com.br>
2021-01-24 09:14:15 -0300

    dinning philosophers

--- TP1/TP1.c
+++ TP1/TP1.c
@@ -1,39 +1,38 @@
-#include "TP1.h"
+//#include "TP1.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 
 // Comando que estou usando para compilar:
 // gcc -pthread -o TP1 TP1.c
 
-int sum; /* esses dados são compartilhados pelo(s) thread(s) */
-void *runner(void *param); /* os threads chamam essa função */
-int main(int argc, char *argv[])
+//mutex 
+pthread_mutex_t lock[5];
+
+typedef enum
 {
+    false = 0,
+    true  = 1,
+} bool;
 
-    pthread_t tid; /* o identificador do thread */
-    pthread_attr_t attr; /* conjunto de atributos do thread */
+#define DETERMINISTIC true
 
-    if (argc != 2) {
-        fprintf(stderr,"usage: a.out <integer value>\n");
-        return -1;
-    }
-    
-    if (atoi(argv[1]) < 0) {
-        fprintf(stderr,"%d must be >= 0\n",atoi(argv[1]));
-        return -1;
-    }
-    
-    /* obtém os atributos default */
-    pthread_attr_init(&attr);
+int sum; /* esses dados são compartilhados pelo(s) thread(s) */
 
-    /* cria o thread */
-    pthread_create(&tid,&attr,runner,argv[1]);
+typedef struct Pessoa_t{
+    int espera;
+    char nome[20];
+} Pessoa_t;
 
-    /* espera o thread ser encerrado */
-    pthread_join(tid,NULL);
-    printf("sum = %d\n",sum);
-}
-    
+typedef struct Forno_t{
+    int tempo_restante;
+    bool liberado;
+} Forno_t;
+
 /* O thread assumirá o controle nessa função */
-void *runner(void *param)
+void *runner(void *param)  /* os threads chamam essa função */
 {
     int i, upper = atoi(param);
     sum = 0;
@@ -103,3 +102,47 @@ int rand_int(){
         ret= 2;
     return ret;
 }
+
+
+
+// Comando que estou usando para compilar:
+// gcc -pthread -o TP1 TP1.c
+
+int sum; /* esses dados são compartilhados pelo(s) thread(s) */
+//void *runner(void *param); /* os threads chamam essa função */
+int main(int argc, char *argv[])
+{
+
+    pthread_t tid; /* o identificador do thread */
+    pthread_attr_t attr; /* conjunto de atributos do thread */
+
+    if (argc != 2) {
+        fprintf(stderr,"usage: a.out <integer value>\n");
+        return -1;
+    }
+    
+    if (atoi(argv[1]) < 0) {
+        fprintf(stderr,"%d must be >= 0\n",atoi(argv[1]));
+        return -1;
+    }
+    Pessoa_t pessoas[10];
+    char nomes_pessoas = {"Raj", "Leonard", "Sheldon", "Howard", "Amy", "Penny",
+                            "Bernardette", "Stuart"}
+                            
+	//initialization
+	for (int i; i < 5; i++)
+	{
+		pthread_mutex_init(&lock[i], NULL);
+	}                            
+                            
+    /* obtém os atributos default */
+    pthread_attr_init(&attr);
+
+    /* cria o thread */
+    pthread_create(&tid,&attr,runner,argv[1]);
+
+    /* espera o thread ser encerrado */
+    pthread_join(tid,NULL);
+    printf("sum = %d\n",sum);
+}
+    
---
+++ TP1/dining_philosophers/dinning_philosophers.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+//mutex 
+pthread_mutex_t lock[5];
+
+//thread 
+void *thr(void *arg)
+{
+	int left, right;
+
+	int i = (int)arg;
+
+	// The first four take the left hand
+	if (i < 4)
+	{
+		left = i;
+		if (i == 0)
+		{
+			right = 4;
+		}
+		else{
+			right = i - 1;
+		}
+	}
+
+	//The fifth one takes the right lock
+	if (i == 4)
+	{
+		left = i-1;
+		right = i;
+	}
+	while (1)
+	{
+
+		//Apply for the lock on the left
+		pthread_mutex_lock(&lock[left]);
+
+		//Apply for the lock on the right
+		if ((pthread_mutex_trylock(&lock[right])) != 0)
+		{
+			pthread_mutex_unlock(&lock[left]);
+			printf(" ==thread  %d ==  Application failed\n", i);
+			continue;
+		}
+
+		//Both sides successfully applied
+		printf("==thread  %d == succesful\n", i);
+
+		//Release the lock
+		pthread_mutex_unlock(&lock[left]);
+		pthread_mutex_unlock(&lock[right]);
+
+		sleep(1);
+	}
+}
+
+int main()
+{
+	//initialization
+	for (int i; i < 5; i++)
+	{
+		pthread_mutex_init(&lock[i], NULL);
+	}
+
+	//Create thread
+	pthread_t dit[5];
+	for (int i = 0; i < 5; i++)
+	{
+		pthread_create(&dit[i], NULL, thr, (void *)i);
+	}
+
+	//Recycle thread
+	for (int i = 0; i < 5; i++)
+	{
+		pthread_join(dit[i], NULL);
+	}
+	//Destruction
+	pthread_exit(NULL);
+
+	return 0;
+}
